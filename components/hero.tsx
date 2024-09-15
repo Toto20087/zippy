f27ac53ef6cb3032d71f58b8778749d8b6cb3ff8
Toto20087 <tomas.rodriguez.moyano@gmail.com>
2024-09-14 21:26:36 -0300

    Hola buenas

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,15 +8,9 @@ import { IoMdMail } from "react-icons/io";
 const Hero = () => {
   return (
     <>
-        <div className='h-full w-full flex items-center justify-center bg-[#0099FF] bg-[url("/fondo.png")] bg-cover bg-no-repeat '>
-            <div className='flex items-start justify-center flex-col space-y-6'>
-                <Image 
-                src="/zippy.svg"
-                width={500}
-                height={500}
-                alt="Zippy Logo"
-                />
-                <h1 className="text-3xl w-1/2 text-white">
+        <div className='h-full w-full flex items-center justify-center bg-[#0099FF] bg-[url("/deskback.svg")] bg-cover bg-no-repeat '>
+            <div className='flex items-center justify-center flex-col space-y-6'>
+                <h1 className="text-8xl w-1/2 text-white">
                     Hacemos que enseñar Tecnología sea simple
                 </h1>
                 <ButtonGrande 
@@ -28,37 +22,6 @@ const Hero = () => {
                     <FaInstagramSquare size={30} color={"#FFFFFF"} className="cursor-pointer" />
                     <FaFacebookSquare size={30} color={"#FFFFFF"} className="cursor-pointer" />
                     <IoMdMail size={30} color={"#FFFFFF"} className="cursor-pointer" />
-                </div>
-            </div>
-            <div className="flex items-center justify-center flex-col space-y-10 relative">
-                <div className="grid grid-cols-2 grid-rows-2 gap-11 animate-flotar">
-                    <Image 
-                        src="/conejo.png"
-                        width={100}
-                        height={100}
-                        alt="Conejo"
-                        className="mb-10"
-                    />
-                    <Image 
-                        src="/codigo.png"
-                        width={100}
-                        height={100}
-                        alt="Codigo"
-                        className="mt-20"
-                    />
-                    <Image 
-                        src="/carita feliz.png"
-                        width={100}
-                        height={100}
-                        alt="Carita Feliz"
-                    />
-                    <Image 
-                        src="/flan.png"
-                        width={100}
-                        height={100}
-                        alt="Flan"
-                        className="mt-20"
-                    />
                 </div>
             </div>
         </div>
